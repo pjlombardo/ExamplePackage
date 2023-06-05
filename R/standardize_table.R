d2eef3ad7d1dca08a5427596daa933027be4123d
pjlombardo <philip.j.lombardo@gmail.com>
2023-06-05 16:43:32 -0400

    "Added a new R function to create a standardization table as per Milo Schield"

---
+++ R/standardize_table.R
@@ -0,0 +1,35 @@
+standardize_table<-function(df,
+                            response,
+                            explanatory,
+                            confounder,
+                            mix = c(.5, .5)){
+    mix_table<-rbind(mix,mix)
+    # print(mix_table)
+
+    newdf<-split(df, df[confounder])
+
+    rates1<- prop.table(table(newdf[[1]][[explanatory]],
+                              newdf[[1]][[response]]),
+                        margin =1)[,1]
+    rates2<- prop.table(table(newdf[[2]][[explanatory]],
+                              newdf[[2]][[response]]),
+                        margin =1)[,1]
+    all_rates<-cbind(rates1,rates2)
+    # print(all_rates)
+
+    conf_table <-prop.table(
+        table(df[[explanatory]], df[[confounder]]),
+        margin = 1
+    )
+    # print(conf_table)
+    unstd_rates<-diag(all_rates%*%t(conf_table))
+    std_rates<-diag(all_rates %*% t(mix_table))
+
+    print(cbind(all_rates[,1],conf_table[,1],
+                all_rates[,2],conf_table[,2],
+                unstd_rates))
+    print(cbind(all_rates[,1],mix_table[,1],
+                all_rates[,2],mix_table[,2],
+                std_rates))
+
+}
